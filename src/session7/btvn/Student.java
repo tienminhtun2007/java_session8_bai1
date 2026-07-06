c497342d5944a6d38d77eaddd93d1f64cec4b046
tienminhtun2007 <tienminhtun2007@gmail.com>
2026-07-07 02:28:47 +0700

    Thêm phương thức displayData

--- src/session7/btvn/Student.java
+++ src/session7/btvn/Student.java
@@ -4,4 +4,10 @@ public class Student {
     String studentId;
     String studentName;
     int age;
+    public void displayData() {
+        System.out.println("========THÔNG TIN SINH VIÊN========");
+        System.out.println("Mã sinh viên: " + studentId);
+        System.out.println("Tên sinh viên: " + studentName);
+        System.out.println("Tuổi: " + age);
+    }
 }
